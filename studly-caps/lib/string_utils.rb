e89f9a90e591d6dc293574b91d51eae8fa3dfe55
Débora <debora.d.r.a@usp.br>
2021-04-01 12:41:26 -0300

    best practice

--- studly-caps/lib/string_utils.rb
+++ studly-caps/lib/string_utils.rb
@@ -1,5 +1,5 @@
 class StringUtils
   def wavefy(string)
-    return string.chars.map.with_index{|c,index| (index.even? ? c.downcase : c.upcase) }.join
+    string.chars.map.with_index{|c,index| (index.even? ? c.downcase : c.upcase) }.join
   end
 end
