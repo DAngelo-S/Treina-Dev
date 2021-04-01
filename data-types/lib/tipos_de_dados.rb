a4f23b66a2f0f189ddc1ee197993cb453119ad50
Débora <debora.d.r.a@usp.br>
2021-04-01 12:36:53 -0300

    best practice

--- data-types/lib/tipos_de_dados.rb
+++ data-types/lib/tipos_de_dados.rb
@@ -1,33 +1,33 @@
 class TiposDeDados
   def ola_mundo
-    return "Olá Mundo"
+    "Olá Mundo"
   end
 
   def um_numero
-    return 1
+    1
   end
 
   def um_decimal
-    return 0.1
+    0.1
   end
 
   def verdadeiro?
-    return true
+    true
   end
 
   def falso?
-    return false
+    false
   end
 
   def array_vazio
-    return []
+    []
   end
 
   def array_com_um_elemento
-    return [1]
+    [1]
   end
 
   def array_com_dois_elementos
-    return [1,2]
+    [1,2]
   end
 end
