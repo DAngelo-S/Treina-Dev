70f9b3eb81f5bbd82a7947295c4639a3123e2a8a
Débora <debora.d.r.a@usp.br>
2021-04-01 16:26:34 -0300

    using map

--- multiply-arrays/lib/arrays.rb
+++ multiply-arrays/lib/arrays.rb
@@ -1,7 +1,7 @@
 class Arrays
   def self.multiplica_antecessor_predecessor(array)
     new_array = []
-    array.each_with_index do |n, i|
+    array.map.with_index do |n, i|
       if(i == 0 && i+1 < array.size)
         new_array[i] = array[i]*array[i+1]
       elsif(i == array.size - 1 && i - 1 >= 0)
